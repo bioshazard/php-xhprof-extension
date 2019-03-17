5f8aaf5091854f69c5e83cdf1273de4ac1f48947
Joe Still <joe@bioshazard.com>
2019-03-17 14:17:10 -0400

    Introduced kill signal toggling for trace operation.

--- xh.prepend.php
+++ xh.prepend.php
@@ -1,6 +1,23 @@
 <?php
 
-tideways_xhprof_enable();
+$tracing = 0;
 
-// $result = tideways_xhprof_disable();
+// Use kill signals to toggle tracing
+pcntl_async_signals(true);
+
+// Toggle tracing with `kill -USR1 $pid`
+// Great for long running CLI jobs when you don't know what they are up to
+pcntl_signal(10, function ($signo) {
+  global $tracing;
+  if ($tracing == 0) {
+    tideways_xhprof_enable();
+      // TODO get cpu amount and mem? FLAGS?
+    $tracing = 1;
+  } else {
+    $result = tideways_xhprof_disable();
+    file_put_contents('/dev/shm/xh.out', serialize($result));
+    $tracing = 0;
+  }
+});
+
 ?>
